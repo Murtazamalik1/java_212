40dd399ec453637b7d813a88c7b40a891bf7ccaf
murtaza <murtazamalik1319@gmail.com>
2022-10-29 22:52:25 +0530

    employee wage uc1

---
+++ employee.java
@@ -0,0 +1,16 @@
+
+
+public class employee {
+
+    public static void main(String[] args) {
+        int full_time_emp = 1;
+        double empcheck = Math.floor(Math.random()  * 10) % 2 ;
+        if (empcheck == full_time_emp)
+            System.out.println("employee is present");
+        else
+            System.out.println("employee is not present");
+
+
+    }
+
+    }
